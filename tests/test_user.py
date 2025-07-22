b4ada72f71e411146233838aacf5c48c5e8c947d
Matthew Lau <lau.cy.matthew@gmail.com>
2025-07-22 21:09:25 +0100

    feat: test create user

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,6 +2,7 @@ import pytest
 import uuid
 from datetime import datetime
 from fastapi.testclient import TestClient
+import json
 
 from api.main import app
 
@@ -20,8 +21,23 @@ def mocked_repository(mocker):
             }
         ],
     )
+    mocker.patch(
+        "api.user.repository.UserRepository.create",
+        return_value=[
+            {
+                "id": uuid.uuid4(),
+                "email": "hello@example.com",
+                "created_at": datetime.now(),
+            }
+        ],
+    )
 
 
 def test_get_users(mocked_repository):
     response = client.get("/api/v1/users")
     assert response.status_code == 200
+
+
+def test_create_user(mocked_repository):
+    response = client.post("/api/v1/users", json={"email": "one@example.com"})
+    assert response.status_code == 201
