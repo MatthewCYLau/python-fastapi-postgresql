c21f1f7e57c6440fc5d91b6fee33aaec7486440d
Matthew Lau <lau.cy.matthew@gmail.com>
2025-08-01 20:18:16 +0100

    feat: validate order product and user IDs

--- api/order/service.py
+++ api/order/service.py
@@ -2,6 +2,9 @@ from api.config.logging import get_logger
 from api.order.models import Order
 from api.order.repository import OrderRepository
 from api.order.schemas import OrderBase, OrderResponse
+from api.product.repository import ProductRepository
+from api.product.service import ProductService
+from api.user.service import UserService
 
 logger = get_logger(__name__)
 
@@ -12,7 +15,16 @@ class OrderService:
         self.repository = OrderRepository(session)
 
     def create_order(self, order_data: OrderBase) -> Order:
-        return self.repository.create(order_data)
+
+        user = UserService(self.session).get_user_by_id(order_data.user_id)
+
+        product_repository = ProductRepository(self.session)
+        product_service = ProductService(product_repository)
+
+        product = product_service.get_product_by_id(order_data.product_id)
+
+        if user and product:
+            return self.repository.create(order_data)
 
     def get_all_orders(self) -> list[OrderResponse]:
         orders = self.repository.get_all()
