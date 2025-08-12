caab0f4c514a5606b7aa255ce3b5e067d1a9997c
Matthew Lau <lau.cy.matthew@gmail.com>
2025-08-12 19:26:38 +0100

    feat: prometheus metrics #8

--- api/main.py
+++ api/main.py
@@ -1,9 +1,18 @@
+import time
 from dotenv import load_dotenv
 import os
 
+from api.metrics.metrics import (
+    REQUEST_COUNT,
+    REQUEST_IN_PROGRESS,
+    REQUEST_LATENCY,
+    update_system_metrics,
+)
+
+
 load_dotenv(".env")
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from api.config.logging import setup_logging, get_logger
 from api.user.views import router as user_router
 from api.product.views import router as product_router
@@ -11,6 +20,11 @@ from api.order.views import router as order_router
 from api.auth.views import router as auth_router
 from api.comment.views import router as comment_router
 from api.config.database import Base, engine
+from prometheus_client import (
+    generate_latest,
+    CONTENT_TYPE_LATEST,
+    REGISTRY,
+)
 
 
 setup_logging()
@@ -37,3 +51,25 @@ async def up():
 @app.get("/ping")
 async def pong():
     return "pong!"
+
+
+@app.middleware("http")
+async def monitor_requests(request: Request, call_next):
+    method = request.method
+    path = request.url.path
+    REQUEST_IN_PROGRESS.labels(method=method, path=path).inc()
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    status = response.status_code
+    REQUEST_COUNT.labels(method=method, status=status, path=path).inc()
+    REQUEST_LATENCY.labels(method=method, status=status, path=path).observe(duration)
+    REQUEST_IN_PROGRESS.labels(method=method, path=path).dec()
+
+    return response
+
+
+@app.get("/metrics")
+async def metrics():
+    update_system_metrics()
+    return Response(generate_latest(REGISTRY), media_type=CONTENT_TYPE_LATEST)
