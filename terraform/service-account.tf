4525767cb43cf20c09a66b7eaed4a3143176586f
Matthew Lau <lau.cy.matthew@gmail.com>
2025-10-13 20:06:58 +0100

    feat: service account condition

--- terraform/service-account.tf
+++ terraform/service-account.tf
@@ -7,10 +7,20 @@ resource "google_project_iam_member" "cloud_sql_instance_user" {
   project = var.project
   role    = "roles/cloudsql.instanceUser"
   member  = "serviceAccount:${google_service_account.cloud_sql_instance_user.email}"
+  condition {
+    title       = "resource_name_equals_cloud_sql_instance_name"
+    description = "Resource name equals ${google_sql_database_instance.this.name}"
+    expression  = "resource.name == 'projects/${var.project}/instances/${google_sql_database_instance.this.name}'"
+  }
 }
 
 resource "google_project_iam_member" "cloud_sql_instance_user_client" {
   project = var.project
   role    = "roles/cloudsql.client"
   member  = "serviceAccount:${google_service_account.cloud_sql_instance_user.email}"
+  condition {
+    title       = "resource_name_equals_cloud_sql_instance_name"
+    description = "Resource name equals ${google_sql_database_instance.this.name}"
+    expression  = "resource.name == 'projects/${var.project}/instances/${google_sql_database_instance.this.name}'"
+  }
 }
